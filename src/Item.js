2467d7c281661771dbbb58bf5a6f6504e9e0fe41
helenjgardner <helen_gardner@hotmail.com>
2019-10-21 11:04:24 +0100

    display uncheck button if not completed using ternary

--- src/Item.js
+++ src/Item.js
@@ -16,8 +16,17 @@ class Item extends React.Component {
             <button className="btn btn-info">Delete</button>
           </div>
           <div className="col-2">
-            {this.props.completed===false && <button className="btn btn-light">Done</button>}
-             {/* if lhs is true then do right hand side of && */}
+            {/* {this.props.completed===false && <button className="btn btn-light">Done</button>} */}
+             {/* if lhs is true then do right hand side of && bit of a trick  */}
+             {/* if this then that */}
+             {/* {this.props.completed===true && <button className="btn btn-light">Uncheck</button>} */}
+             {/* two poss situations then ternary */}
+
+             {this.props.completed ? 
+                   <button className="btn btn-light">Uncheck</button> : 
+                   <button className="btn btn-light">Done</button>}
+
+
           </div>
         </div>
       </li>
