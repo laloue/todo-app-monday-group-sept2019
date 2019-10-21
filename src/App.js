f058cea39f05fde354a7da4ba664f746555c8cce
helenjgardner <helen_gardner@hotmail.com>
2019-10-21 12:12:46 +0100

    used state to render tasks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Item from "./Item";
 import "./App.css";
 
 class App extends React.Component {
+  state={
+    tasks:[
+      {text:"walk the cat", completed:true, date:"2019-10-20"},
+      {text:"buy oats", completed:false, date:"2019-10-21"},
+      {text:"learn React", completed:false, date:"2019-10-16"},
+      {text:"wash slippers", completed:true, date:"2019-10-10"},
+      {text:"de-flea the cat", completed:true, date:"2019-10-31"},
+      {text:"shop", completed:true, date:"2019-10-16"}
+    ]
+  }
   render() {
     return (
       <div className="container">
@@ -15,13 +25,11 @@ class App extends React.Component {
             <AddItem />
           </div>
           <div className="col-12 col-lg-6">
-            <ItemCount count={5} />
+            <ItemCount count={this.state.tasks.length} />
             <ul id="itemList">
-              <Item text="walk the cat" completed={true}/>
-              <Item text="buy oats" completed={false}/>
-              <Item text="learn React" completed={false}/>
-              <Item text="wash slippers" completed={false}/>
-              <Item text="de-flea the cat" completed={true} />
+              {this.state.tasks.map(item => {
+                 return <Item text={item.text} completed={item.completed} date={item.date}/>
+              })}
             </ul>
           </div>
         </div>
