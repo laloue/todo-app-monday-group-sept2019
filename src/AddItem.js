7352f034cc26e5510874d23ca0a4c278e8be017b
helenjgardner <helen_gardner@hotmail.com>
2019-10-22 08:56:56 +0100

    added functionality to add button

--- src/AddItem.js
+++ src/AddItem.js
@@ -2,6 +2,29 @@ import React from "react";
 import "./AddItem.css";
 
 class AddItem extends React.Component {
+  // initial state of our component
+  // always an object
+  // keep track of text typed
+  // can have state of a component not just whole app
+  state = {
+    newTaskText: ""
+  }
+
+  updateTaskText = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      newTaskText: event.target.value
+    })
+  }
+  
+  handleClick=() =>{
+    // check not empty though
+   this.props.addTaskFunc(this.state.newTaskText);
+   this.setState({
+     newTaskText: ""
+    })
+  }
+
   render() {
     // JSX is NOT HTML
     return (
@@ -9,11 +32,13 @@ class AddItem extends React.Component {
         <input
           id="addItemInput"
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="E.g. take out bins"
+          value={this.state.newTaskText}
+          onChange={this.updateTaskText}
         />
         <div className="mt-3 mb-3">
-          <button id="addButton" className="btn">
+          <button id="addButton" className="btn" onClick={this.handleClick}>
             Add to list
           </button>
         </div>
